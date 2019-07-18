4bc930506873944957005fa3182140a9e2e92ba3
cuamckuu <sasha23969@gmail.com>
2019-07-18 12:41:07 +0300

    License moved to separate file

--- headers/tools.h
+++ headers/tools.h
@@ -1,25 +1,3 @@
-/*
-  Copyright (C) 2014 Alexis Guillard, Maxime Marches, Thomas Brunner
-
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-  File written for the requirements of our MSc Project at the University of Kent, Canterbury, UK
-
-  Retrieves information available from EMV smartcards via an RFID/NFC reader.
-  Both tracks are printed then track2 is parsed to retrieve PAN and expiry date.
-  The paylog is parsed and showed as well.
-
-  All these information are stored in plaintext on the card and available to anyone.
-
-  Requirements:
-  libnfc (>= 1.7.1) -> For later versions, please update the pn52x_transceive() prototype if needed, as it is not included in nfc.h
-
-*/
-
 #ifndef __TOOLS_HH__
 # define __TOOLS_HH__
 
