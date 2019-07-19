b1d2bfe135b5722e23e127e6b048498991eac9b1
cuamckuu <sasha23969@gmail.com>
2019-07-19 23:18:41 +0300

    Wrap functions added. Exceptions added

--- device_nfc.cpp
+++ device_nfc.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <list>
+#include <cstring>
 
 #include "headers/device_nfc.h"
 
@@ -7,27 +9,67 @@ nfc_context *DeviceNFC::context = nullptr;
 DeviceNFC::DeviceNFC() {
     nfc_init(&context);
     if (context == nullptr) {
-        std::cerr << "Unable to init libnfc (malloc)" << std::endl;
-        exit(EXIT_FAILURE);
+        throw std::runtime_error("Unable to init libnfc (malloc)");
     }
 
     pnd = nfc_open(context, nullptr);
-
     if (pnd == nullptr) {
-        std::cerr << "Unable to open NFC device." << std::endl;
         nfc_exit(context);
-        exit(EXIT_FAILURE);
+
+        throw std::runtime_error("Unable to open NFC device");
     }
 
     if (nfc_initiator_init(pnd) < 0) {
-        nfc_perror(pnd, "nfc_initiator_init");
         nfc_close(pnd);
         nfc_exit(context);
-        exit(EXIT_FAILURE);
+
+        throw std::runtime_error(nfc_strerror(pnd));
+    }
+}
+
+bool DeviceNFC::pool_target() {
+    uint8_t const uiPollNr = 1;
+    uint8_t const uiPeriod = 2;
+
+    size_t const szModulations = 1;
+    nfc_modulation const nmModulations[szModulations] = {
+        {NMT_ISO14443A, NBR_106}
+    };
+
+    int err = nfc_initiator_poll_target(pnd, nmModulations, szModulations, uiPollNr, uiPeriod, &nt);
+
+    if (err > 0) {
+        return true;
     }
 
-    std::cout << "NFC reader: " << nfc_device_get_name(pnd) << " opened."
-              << std::endl;
+    if (err < 0) {
+        nfc_close(pnd);
+        nfc_exit(context);
+
+        throw std::runtime_error(nfc_strerror(pnd));
+    }
+
+    return false;
+}
+
+void DeviceNFC::print_target_info() {
+    char *info;
+    str_nfc_target(&info, &nt, true);
+
+    std::cout << info;
+    nfc_free(info);
+}
+
+std::string DeviceNFC::get_name() {
+    return std::string(nfc_device_get_name(pnd));
+}
+
+APDU DeviceNFC::execute_command(byte_t const *command, size_t size, char const *name) {
+    return ApplicationHelper::executeCommand(pnd, command, size, name);
+}
+
+std::list<Application> DeviceNFC::load_applications_list() {
+    return ApplicationHelper::getAll(pnd);
 }
 
 DeviceNFC::~DeviceNFC() {
--- headers/device_nfc.h
+++ headers/device_nfc.h
@@ -4,14 +4,38 @@
 extern "C" {
     #include <nfc/nfc-types.h>
     #include <nfc/nfc.h>
+
+    #ifndef PN32X_TRANSCEIVE
+    #define PN32X_TRANSCEIVE
+    int pn53x_transceive(struct nfc_device *pnd, const uint8_t *pbtTx,
+                         const size_t szTx, uint8_t *pbtRx, const size_t szRxLen,
+                         int timeout);
+    #endif // PN32X_TRANSCEIVE
 }
+
+#include <list>
+
+#include "tools.h"
+#include "applicationhelper.h"
 
 class DeviceNFC {
   public:
     DeviceNFC();
+    bool pool_target();
+    std::string get_name();
+    void print_target_info();
+
     ~DeviceNFC();
 
+    std::list<Application> load_applications_list();
+    APDU execute_command(byte_t const *command, size_t size, char const *name);
+
   private:
+    byte_t abtRx[MAX_FRAME_LEN] = {};
+    int szRx = 0;
+
+  public:
+    nfc_target nt;
     nfc_device *pnd = nullptr;
     static nfc_context *context;
 };
