72969ffe262ac55dc6dc7f7bf8e57653ab2da83b
cuamckuu <sasha23969@gmail.com>
2019-07-22 02:30:53 +0300

    Store it as usage example for now

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@ extern "C" {
 
 #include <iostream>
 #include <vector>
+#include <chrono>
+#include <thread>
 
 #include "headers/device_nfc.h"
 #include "headers/applicationhelper.h"
@@ -20,10 +22,32 @@ std::vector<CCInfo> extract_information(DeviceNFC &device, std::vector<Applicati
 
         infos[i].parse_response(app, res);
         infos[i].read_record(device);
-        infos[i].extractLogEntries(device);
+        //infos[i].extractLogEntries(device);
     }
 
+
     return infos;
+}
+
+void get_processing_options(DeviceNFC &device) {
+    APDU gpo;
+    byte_t select_app[256] = {0};
+
+    byte_t size = 0;
+    for (byte_t len = 0; len <= 32; len++) {
+        byte_t command[] = { 0x40, 0x01, 0x80, 0xA8, 0x00, 0x00, len+2, 0x83, len};
+        size = sizeof(command) + len + 1;
+        memcpy(select_app, command, size);
+
+        APDU res = device.execute_command(select_app, size, "GET PROCESSING OPTIONS");
+
+        byte_t status[2] = {res.data[res.size-2], res.data[res.size-1]};
+        if (status[0] == 0x90 && status[1] == 0x00) {
+            gpo = res;
+            break;
+        }
+    }
+
 }
 
 int main() {
@@ -34,14 +58,15 @@ int main() {
         DeviceNFC device;
         std::cout << "NFC reader: " << device.get_name() << " opened.\n";
 
-        bool is_ok = device.pool_target();
-        if (is_ok) {
-        //    device.print_target_info();
+        while (!device.pool_target()) {
+            std::cout << "Searching..." << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
         }
 
         std::vector<Application> list = device.load_applications_list();
 
         std::vector<CCInfo> infos = extract_information(device, list);
+        //get_processing_options(device);
 /*
         for (Application &app: list) {
             std::cout << "Name: " << app.name << std::endl;
